c7bdb4c8d2570fedb82346f400544497460bf5d6
tricong2011 <tricong2011@gmail.com>
2018-07-17 14:40:32 +0700

    connect_string

--- ABI/Analyzer/LoadQuestions.cs
+++ ABI/Analyzer/LoadQuestions.cs
@@ -13,13 +13,13 @@ namespace ABI
     public abstract class LoadQuestions
     {
         public const string CONNECTION_STRING = @"Data Source=DESKTOP-R7EB7OG\SQLEXPRESS;Initial Catalog=abi_db;Integrated Security=True";
-
+        public const string CONNECTION_STRING_C = @"Data Source=DESKTOP-O33GLSQ\SQLEXPRESS;Initial Catalog=abi_db;Integrated Security=True";        
         SqlConnection conn;
 
         public SqlConnection Initialize()
         {
             conn = new SqlConnection();
-            conn.ConnectionString = CONNECTION_STRING;
+            conn.ConnectionString = CONNECTION_STRING_C;
             conn.Open();
             return conn;
         }
